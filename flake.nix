6e5d67920085ef0d133d4eb92c8f365442850be5
Desant <anturated@gmail.com>
2026-04-12 20:16:33 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "make your launch args shorter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosModules.default =
+        { pkgs, ... }:
+        {
+          imports = [ ./kaled.nix ];
+
+          environment.systemPackages = [
+            (pkgs.callPackage ./kale.nix { })
+          ];
+        };
+    };
+}
